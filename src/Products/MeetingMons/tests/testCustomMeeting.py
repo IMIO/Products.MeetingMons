3160459e87a1f54968dcc1a5a5d93e341addbd4c
André Nuyens <andre.nuyens@imio.be>
2016-02-29 12:26:01 +0000

    add printing test

--- src/Products/MeetingMons/tests/testCustomMeeting.py
+++ src/Products/MeetingMons/tests/testCustomMeeting.py
@@ -25,6 +25,8 @@
 from Products.MeetingMons.tests.MeetingMonsTestCase import \
     MeetingMonsTestCase
 
+from DateTime import DateTime
+
 
 class testCustomMeeting(MeetingMonsTestCase):
     """
@@ -36,3 +38,63 @@ class testCustomMeeting(MeetingMonsTestCase):
           Already tested in MeetingMons.tests.testMeeting.py
         '''
         pass
+
+    def test_GetPrintableItems(self):
+        meetingConfigCollege = self.meetingConfig.getId()
+        self.changeUser('pmManager')
+
+        #makes the college meeting
+        self.setMeetingConfig(meetingConfigCollege)
+        meetingDate = DateTime('2019/09/09 19:19:19')
+        collegeMeeting = self._createMeetingWithItems(meetingDate=meetingDate)
+
+        #create another point in the development category to help the enum test
+        item5 = self.create('MeetingItem')
+        item5.setProposingGroup('vendors')
+        item5.setPrivacy('public')
+        item5.setCategory('development')
+        item5.setDecision('<p>A decision</p>')
+        self.presentItem(item5)
+
+        #build the list of uids
+        collegeItemUids = []
+        for item in collegeMeeting.getItems(ordered=True):
+            collegeItemUids.append(item.UID())
+
+        items = collegeMeeting.adapted().getPrintableItems(
+            itemUids=collegeItemUids
+        )
+        #Are all the categories there?
+        self.assertTrue([item.getId() for item in items] ==
+                        ['recItem1', 'recItem2', 'o3', 'o5', 'o2', 'o4', 'o6', 'o7'])
+
+    def test_GetPrintableItemsByCategory(self):
+        meetingConfigCollege = self.meetingConfig.getId()
+        self.changeUser('pmManager')
+
+        #makes the college meeting
+        self.setMeetingConfig(meetingConfigCollege)
+        meetingDate = DateTime('2019/09/09 19:19:19')
+        collegeMeeting = self._createMeetingWithItems(meetingDate=meetingDate)
+
+        #create another point in the development category to help the enum test
+        item5 = self.create('MeetingItem')
+        item5.setProposingGroup('vendors')
+        item5.setPrivacy('public')
+        item5.setCategory('development')
+        item5.setDecision('<p>A decision</p>')
+        self.presentItem(item5)
+
+        #build the list of uids
+        collegeItemUids = []
+        for item in collegeMeeting.getItems(ordered=True):
+            collegeItemUids.append(item.UID())
+
+        sub_list = collegeMeeting.adapted().getPrintableItemsByCategory(
+            itemUids=collegeItemUids
+        )
+        #Are all the categories there?
+        self.assertTrue(sub_list[0][0].getId() == 'developers')
+        self.assertTrue([item.getId() for item in sub_list[0][1:]] == ['recItem1', 'recItem2', 'o3', 'o5'])
+        self.assertTrue(sub_list[1][0].getId() == 'vendors')
+        self.assertTrue([item.getId() for item in sub_list[1][1:]] == ['o2', 'o4', 'o6', 'o7'])
